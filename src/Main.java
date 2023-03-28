b6ee98f52e8d055e0e9c731b981484f528bd98dd
alumnoDAW <albertocriadoortega@gmail.com>
2023-03-28 08:56:48 +0200

    yes

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public void integers(){
+        for (int i = 0; i < 64255; i++) {
+            System.out.println(i);
+        }
+    }
 }
