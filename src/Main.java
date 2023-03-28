8d1f1ced94efb075d560c01b86f04a7b923ac3f6
alumnoDAW <albertocriadoortega@gmail.com>
2023-03-28 09:28:07 +0200

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        integers();
-        integersss();
-        integerssss();
-        integerss();
+        System.out.println("Hello world!");
     }
-
-
-    public static void integers(){
-        for (int i = 0; i < 10255; i++) {
-            System.out.println(i);
-        }
-    }
-
-    public static void integersss(){
-        for (int i = 0; i < 10255; i++) {
-            System.out.println(i);
-        }
-    }
-
-
-    public static void integerss(){
-        for (int i = 0; i < 10255; i++) {
-            System.out.println(i);
-        }
-    }
-
-    public static void integerssss(){
-        for (int i = 0; i < 10255; i++) {
-            System.out.println(i);
-        }
-    }
-
-
 }
